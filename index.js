7aa7a147738a4ee449b020a4b0ab055a9d890b13
derek williams <derek61@gmail.com>
2023-10-26 14:17:36 -0600

    Completed 3.2: Phonebook backend step2

--- index.js
+++ index.js
@@ -4,30 +4,38 @@ const app = express()
 app.use(express.json())
 
 const persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
   },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
   },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
   },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
 ]
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  response.send(
+    `<html><p>Phonebook has info for ${
+      persons.length
+    } people</p><p>${new Date().toUTCString()}</p></html>`
+  )
 })
 
 const PORT = 3001
